bfbe20952f6b537c6af377efaf11e50a3df972e0
杨宇忠 <yangyuzhong@yangyuzhongs-MacBook-Pro.local>
2017-04-20 21:33:20 +0800

    Initial commit

---
+++ src/com/yyz/test/Test.java
@@ -0,0 +1,10 @@
+package com.yyz.test;
+
+/**
+ * Created by yangyuzhong on 4/20/17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("test");
+    }
+}
